84afdd2999d73e98c2c6881c2f6778d5d87d02fc
Michał Sidor <public+git@meekchopp.es>
2021-06-02 16:41:59 +0200

    Increase keepalive interval

--- frontend/uwu.ts
+++ frontend/uwu.ts
@@ -38,7 +38,7 @@ class Metro {
     this.readyTime = new Date();
     this.client = new Paho.MQTT.Client("wss://mqtt.koguma.iscute.ovh/mqtt", "metro-bieszczady");
     this.client.onMessageArrived = this.onMessageArrived.bind(this);
-    this.client.connect({onSuccess: this.onMqttConnect.bind(this), userName: "public", password: "public"});
+    this.client.connect({onSuccess: this.onMqttConnect.bind(this), userName: "public", password: "public", keepAliveInterval: 600});
   }
 
   createPlayer(): void {
